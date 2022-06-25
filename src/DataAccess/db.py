9d6106fc6d46f9013530470fd1f8b30dd2801ab4
Roycard0828 <gamr_0111@outlook.es>
2022-06-24 21:09:00 -0500

    Data base configurations are ready #1

---
+++ src/DataAccess/db.py
@@ -0,0 +1,21 @@
+""" Database configurations and access """
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+import psycopg2
+
+from db_data_access import data_access
+
+database_name = data_access['name']
+username = data_access['username']
+password = data_access['password']
+host = data_access['host']
+port = data_access['port']
+
+url_connection = "postgresql+psycopg2://{}:{}@{}:{}/{}".format(username, password, host, port, database_name)
+connection = create_engine(url_connection)
+
+Session = sessionmaker(bind=connection)
+session = Session()
+
+base = declarative_base()
---
+++ src/DataAccess/db_data_access.py
@@ -0,0 +1,7 @@
+data_access = {
+    'name': 'soccerleague',
+    'username': 'rodrigogm',
+    'password': '011128',
+    'port' : 5432,
+    'host': 'localhost'
+}
